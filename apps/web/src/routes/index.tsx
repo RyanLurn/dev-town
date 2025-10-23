78952d77e9270a905844263aaa7417c1175759c9
Ryan Luong <lumintriet@gmail.com>
2025-10-23 11:45:52 +0700

    chore(web-ui): add button component from shadcn/ui

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  return <h1 className="bg-red-500 text-white p-4">Welcome to Dev Town</h1>;
+  return (
+    <div className="flex h-screen w-screen flex-col gap-4 items-center justify-center">
+      <h1>Welcome to Dev Town</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
