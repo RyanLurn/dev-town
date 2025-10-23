5943b39f56250c46072d620b49025992b17ff421
Ryan Luong <lumintriet@gmail.com>
2025-10-23 11:19:08 +0700

    chore(web-ts): create path alias for src

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter } from "@tanstack/react-router";
-import { NotFound } from "./components/utils/not-found";
-import { routeTree } from "./routeTree.gen";
+import { NotFound } from "@/components/utils/not-found";
+import { routeTree } from "@/routeTree.gen";
 
 export function getRouter() {
   const router = createRouter({
